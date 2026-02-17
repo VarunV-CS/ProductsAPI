1a97a37d0400ad85c680baf0276eb9587baa0157
VarunV <varun.vanam@cestasoft.com>
2026-02-17 16:46:33 +0530

    Deploy test

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  PORT: process.env.PORT || 4000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/m1cart',
+  PORT: process.env.PORT || 8000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://admin:34tD1zgy4Mgf8C60@cluster0.xxxxx.mongodb.net/M1cart?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'm1cart-secret-key-change-in-production-2024',
 };
 
