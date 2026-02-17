b24f5a0f283ee3f6517ae4e96129bd06fd24965e
VarunV <varun.vanam@cestasoft.com>
2026-02-17 16:50:32 +0530

    Tested 2 for DB URL

--- config/db.js
+++ config/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 import config from "./index.js";
 
 const connectDB = async () => {
+  if (!config.MONGODB_URL) {
+    console.error("MONGODB_URL is not defined in environment variables");
+    process.exit(1);
+  }
   try {
     await mongoose.connect(
       config.MONGODB_URL
--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 8000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://admin:34tD1zgy4Mgf8C60@cluster0.xxxxx.mongodb.net/M1cart?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'm1cart-secret-key-change-in-production-2024',
 };
 
